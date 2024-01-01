5367579ca5b81a3118d801db990f3a4b053113fe
Vismaya <svismaya20@gmail.com>
2024-01-01 19:10:16 +0530

    Solution for anagram

--- assignments-master/01-js/easy/anagram.js
+++ assignments-master/01-js/easy/anagram.js
@@ -5,7 +5,21 @@
 */
 
 function isAnagram(str1, str2) {
-  
+  let m = new Map();
+  for (let i of str1) {
+    m.set(i.toLowerCase(), (m.get(i.toLowerCase()) || 0) + 1);
+    console.log(i);
+  }
+  for (let i of str2) {
+    console.log(i);
+    if (!m.has(i.toLowerCase())) return false;
+    else if (m.get(i.toLowerCase()) === 0) return false;
+    else m.set(i.toLowerCase(), m.get(i.toLowerCase()) - 1);
+  }
+  for(let i of m.values()){
+    if(i!==0)return false;
+  }
+  return true;
 }
 
 module.exports = isAnagram;
